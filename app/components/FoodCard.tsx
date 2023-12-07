bbdc6f98998a17ca8af0b5703f3b502e7c3b0916
William Leaver <william.leaver@snhu.edu>
2023-12-07 09:26:17 -0600

    changed styling a bit and maybe fixed photos

--- app/components/FoodCard.tsx
+++ app/components/FoodCard.tsx
@@ -18,11 +18,11 @@ const FoodCard: React.FC<FoodCardProps> = ({ food }) => {
     return (
         <div className="card rounded-3xl bg-base-100 w-full h-full">
             <Image className="border-4 border-base-100 rounded-2xl" src={food.imagePath} width={500} height={500} alt="Breakfast hash" />
-            <div className='flex justify-center p-5'>
+            <div className='flex p-5'>
                 {food.foodLink == "" ? (
-                    <h1 className="font-bold text-white bg-slate-700 rounded-2xl p-5">{food.foodName}</h1>
+                    <h1 className="font-bold text-white bg-slate-700 rounded-2xl p-5 w-full">{food.foodName}</h1>
                 ) : (
-                    <Link className="font-bold text-white bg-teal-700 rounded-2xl p-5 focus:animate-pop" href={food.foodLink}>{food.foodName}</Link>
+                    <Link className="font-bold text-white bg-teal-700 rounded-2xl p-5 focus:animate-pop w-full h-full" href={food.foodLink}>{food.foodName}</Link>
                 )}
                 
             </div>
