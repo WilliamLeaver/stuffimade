1b92dcd23e58f35bbb1f9ca9e0e7f370e5614adf
William Leaver <william.leaver@snhu.edu>
2023-12-30 01:38:02 -0600

    updated more info on CV + layout change

--- app/personal/page.tsx
+++ app/personal/page.tsx
@@ -3,47 +3,69 @@ import Image from 'next/image'
 const space = "                               ";
 const page = () => {
   return (
-    <div className="h-screen w-full text-white rounded-2xl flex flex-col sm:justify-center items-center">
+    <div className="h-full sm:w-full text-white rounded-2xl flex flex-col sm:justify-center items-center">
       <div className='p-4 rounded-2xl bg-base-100'>
         <p className='font-bold text-6xl text-center underline underline-offset-2 decoration-2 decoration-wavy'>Hey look at me! This is my Info</p>
-        <div className="w-full h-36"/>
-        <div className="join join-vertical w-full">
-          <div className="collapse collapse-arrow join-item border-b-2  border-dotted">
-            <input type="radio" name="stats"/>
+        <div className=" h-36"/>
+        <div className="join join-vertical w-full h-full">
+          <div className="collapse collapse-arrow join-item border-b-2  border-dotted hover:bg-base-300">
+            <input type="checkbox" name="stats"/>
             <div className='collapse-title w-min rounded-3xl font-bold text-4xl text-left'>Education</div>
             <div className='collapse-content'>
 
-              <p className='px-3 font-semibold'>Southern New Hampshire University | Manchester, NH</p>
-              <p className='font-extrabold text-right'>06/2021-10/2023</p>
-              <p className='px-3 italic'>B.S. Computer Science w/conc in Software Engineering</p>
+              <p className='px-3 text-xl font-semibold'>Southern New Hampshire University | Manchester, NH</p>
+              <p className='font-extrabold sm:text-right'>06/2021-10/2023</p>          
+              <p className='px-3 italic font-medium'>B.S. Computer Science w/conc in Software Engineering</p>
+             
               <div className='border-b my-2'/>
-              <p className='px-3 font-semibold'>Southern New Hampshire University | Manchester, NH</p>
-              <p className='font-extrabold text-right'>01/2017-12/2019</p>
-              <p className='px-3 italic'>Associates of Science in Information Technology</p>
+             
+              <p className='px-3 text-xl font-semibold'>Southern New Hampshire University | Manchester, NH</p>
+              <p className='font-extrabold sm:text-right'>01/2017-12/2019</p>
+              <p className='px-3 italic font-medium'>Associates of Science in Information Technology</p>
             
             </div>
           </div>
-          <div className="collapse collapse-arrow join-item">
-            <input type="radio" name="stats"/>
+
+          <div className="collapse collapse-arrow join-item hover:bg-base-300">
+            <input type="checkbox" name="stats"/>
             <div className='collapse-title w-min rounded-3xl font-bold text-4xl text-left'>Experience</div>
             <div className='collapse-content'>
 
-              <p className='px-3 font-semibold'>VantagePoint Ai LLC | Remote (Based out of Tampa, FL)</p>
-              <p className='font-extrabold text-right'>04/2022-Present</p>
-              <p className='px-3 italic'>Software Support Level Two</p>
+              <p className='px-3 text-xl font-semibold'>VantagePoint Ai LLC | Remote (Based out of Tampa, FL)</p>
+              <p className='font-extrabold sm:text-right'>04/2022-Present</p>
+              <p className='px-3 italic font-medium'>Software Support Level Two</p>
               
+              <p className='px-5 font-light'>› Develop and implement tools within my daily workflow to mitigate human errors & and enhance operational efficiency, frequently outperforming team metrics by up to 20%.</p>
+              <p className='px-5 font-light'>› Create and oversee tickets generated from emails, calls, and voicemails, ensuring accurate tracking of customer interactions.</p>
+              <p className='px-5 font-light'>› Facilitate customer licensing and software customization in alignment with their selected markets and indicators.</p>
+              <p className='px-5 font-light'>› Exceeded onboarding expectations by upskilling within two weeks of joining to address immediate company needs.</p>
+              <p className='px-5 font-light'>› Deliver consistency by exceeding monthly targets, generating ~$40k in revenue.</p>
+                
+
               <div className='border-b my-2'/>
-              <p className='px-3 font-semibold'>Best Buy | Menifee, CA</p>
-              <p className='font-extrabold text-right'>09/2021-04/2022</p>
-              <p className='px-3 italic'>Sales Associate/Customer Service/Inventory Management</p>
+              
+              <p className='px-3 text-xl font-semibold'>Best Buy | Menifee, CA</p>
+              <p className='font-extrabold sm:text-right'>09/2021-04/2022</p>
+              <p className='px-3 italic font-medium'>Sales Associate/Customer Service/Inventory Management</p>
+              
+              <p className='px-5 font-light'>› Employed problem-solving abilities to provide solutions to customers.</p>
+              <p className='px-5 font-light'>› Accomplished cross-training across multiple departments.</p>
+              <p className='px-5 font-light'>› Managed customer interactions to promote resolution and satisfaction.</p>
+                
               <div className='border-b my-2'/>
-              <p className='px-3 font-semibold'>Evertek | Temecula, CA</p>
-              <p className='font-extrabold text-right'>09/2021-04/2022</p>
-              <p className='px-3 italic'>Sales Associate/Customer Service/Inventory Management</p>
+              
+              <p className='px-3 text-xl font-semibold'>Evertek | Temecula, CA</p>
+              <p className='font-extrabold sm:text-right'>01/2019-07/2020</p>          
+              <p className='px-3 italic font-medium'>Repair Technician</p>
+           
+              <p className='px-5 font-light'>› Serviced and repaired various Windows and Apple products.</p>
+              <p className='px-5 font-light'>› Utilized ticketing software to meticulously document repair processes.</p>
+              <p className='px-5 font-light'>› Guaranteed optimal task execution by excelling in time management skills.</p>
+                
             </div>
           </div>
-          <div className="collapse collapse-arrow join-item border-t-2  border-dotted">
-            <input type="radio" name="stats"/>
+          <div className="collapse collapse-arrow join-item border-t-2 border-dotted hover:bg-base-300">
+            <input type="checkbox" name="stats"/>
             <div className='collapse-title w-min rounded-3xl font-bold text-4xl text-left'>Skills</div>
             <div className='collapse-content'>
               <p className='px-3'>EVERYTHING</p>
