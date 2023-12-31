aff6d7d33fbeb94262d12f44d1eaa1fb94e22bb4
William Leaver <william.leaver@snhu.edu>
2023-12-31 09:54:33 -0600

    updates to the styling for project cards.

--- app/components/WebCard.tsx
+++ app/components/WebCard.tsx
@@ -21,34 +21,34 @@ let Web: Website = {
 
 const WebCard = () => {
   return (
-    <div className="card my-2 p-3 flex-row rounded-3xl bg-base-100 w-1/2 h-1/2">
-      <div className="flex flex-col justify-around">
-
+    <div className="card my-2 p-3 flex-col 2xl:flex-row rounded-3xl bg-base-100 w-full h-full">
+      <div className="flex flex-col items-center justify-center">
+        {/* Start of carousel*/}
+        <Link href={Web.WebsiteLink} className="w-full h-min bg-teal-700 rounded-2xl m-2 p-2 text-center text-white whitespace-nowrap font-bold focus:animate-pop">{Web.Title}</Link>
+        <div className="flex flex-row items-center p-2 rounded-2xl  text-center text-black">
+          {Web.ImagePaths.length > 1 ? (
+          <div className="mr-2 w-0 invisible sm:visible sm:w-auto">
+            <a href="#slide4" className="btn btn-circle bg-teal-700 text-white">❮</a>
+          </div>
+          ) : (<div className="w-0 invisible"/>)}
+          <div className="carousel flex items-center">
+            <Image src={Web.ImagePaths[0]} alt="Website Image should be here" width={3000} height={3000} className="w-full rounded-2xl" />         
+          </div>
+          {Web.ImagePaths.length > 1 ? (
+          <div className="mr-2 w-0 invisible sm:visible sm:w-auto">
+            <a href="#slide4" className="btn btn-circle bg-teal-700 text-white">❮</a>
+          </div>
+          ) : (<div className="w-0 invisible"/>)}
+        </div>
+      </div>
+      <div className="flex flex-col basis-1/2 items-center justify-evenly">
         {/* Start of Description*/}
         <div className="h-min p-10 rounded-2xl bg-slate-700 text-white">{Web.Description}</div>
         {/* Start of Technologies*/}
-
-        <div className="w-min mx-auto rounded-2xl bg-slate-700 mb-2 p-2 text-center text-xl font-bold text-white whitespace-nowrap" >Technologies Used</div>
-        <div className="rounded-2xl h-min bg-slate-700 flex justify-center items-center">
-          <Link href="https://tailwindcss.com/docs/installation" className="btn mx-2 h-20 w-20 p-2 btn-circle bg-teal-700 border-teal-700"><Image className="" src={Web.TechnologiesPaths[0]} alt="T" width={200} height={200}></Image></Link>
+        <div className="rounded-2xl w-min h-min bg-slate-700 flex mt-4 p-10">
+          <Link href="https://tailwindcss.com/docs/installation" className="btn mx-2 h-20 w-20 p-2 btn-circle bg-teal-700 border-teal-700 focus:animate-pop"><Image className="" src={Web.TechnologiesPaths[0]} alt="T" width={200} height={200}></Image></Link>
           <Link href="https://tailwindcss.com/docs/installation" className="btn mx-2 h-20 w-20 p-2 btn-circle bg-teal-700 border-teal-700"><Image className="" src={Web.TechnologiesPaths[0]} alt="T" width={200} height={200}></Image></Link>
         </div>
-      </div>
-      <div className="flex flex-col items-center">
-      {/* Start of carousel*/}
-      <Link href={Web.WebsiteLink} className="w-min h-min bg-teal-700 rounded-2xl m-2 p-2 text-left text-white whitespace-nowrap font-bold ">{Web.Title}</Link>
-      <div className="col-span-2 p-2 rounded-2xl  text-center text-black">
-        <div className="carousel flex items-center">
-          <div className="mr-2">
-            <a href="#slide4" className="btn btn-circle bg-teal-700 text-white">❮</a>
-          </div>
-          <img src={Web.ImagePaths[0]} className="w-full" />
-          <div className="ml-2">
-            <a href="#slide2" className="btn btn-circle bg-teal-700 text-white">❯</a>
-          </div>
-        </div>
-      </div>
-      
       </div>
     </div>
   )
--- app/website/page.tsx
+++ app/website/page.tsx
@@ -3,7 +3,7 @@ import WebCard from '../components/WebCard'
 
 const page = () => {
   return (
-    <div className=''>
+    <div className='lg:grid lg:grid-cols-2 lg:gap-5'>
         <WebCard/>
         <WebCard/>
         <WebCard/>
