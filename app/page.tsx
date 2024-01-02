de93830a0288d32bbd3484dc0109be3dc0ca0b7e
William Leaver <william.leaver@snhu.edu>
2024-01-01 19:07:26 -0600

    updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="w-full h-min p-2 rounded-2xl bg-base-100 text-center font-medium text-4xl text-white">
+      Hello, Welcome to Stuff I Made. This is my Personal Project/Portfolio/Time-Filler Thing. Check out some Stuff I Made using the buttons above.
     </main>
   )
 }
