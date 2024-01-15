873fd2723219a70ffd8121c3bccf1d4b8e985b6b
William Leaver <william.leaver@snhu.edu>
2024-01-15 15:55:02 -0600

    fixed some mobile styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="w-full h-min p-2 rounded-2xl bg-base-100 text-center font-medium text-4xl text-white">
-      Hello, Welcome to Stuff I Made. This is my Personal Project/Portfolio/Time-Filler Thing. Check out some Stuff I Made using the buttons above.
+    <main className="w-full whitespace-wrap h-min p-2 rounded-2xl bg-base-100 text-center font-medium text-4xl text-white">
+      Hello, Welcome to Stuff I Made. This is my Personal Project / Portfolio / Time-Filler Thing. Check out some Stuff I Made using the buttons above.
     </main>
   )
 }
